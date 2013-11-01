bd065dadb2dff2a8a9e4e11f7efeef342c607c7c
sumanta rout <sumantar@cybage.com>
2013-11-01 16:44:24 +0530

    Added a sample code for unit testing purpose

---
+++ ProjectWithCocoapod/Stack.h
@@ -0,0 +1,20 @@
+//
+//  Stack.h
+//  StackApplication
+//
+//  Created by Sumanta Rout on 6/29/10.
+//  Copyright 2010 Cybage. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Stack : NSObject {
+	
+	NSMutableArray *stackArray;	
+}
+
++(Stack *)sharedInstance;
+-(void) push:(id)object;
+-(id) pop;
+
+@end
---
+++ ProjectWithCocoapod/Stack.m
@@ -0,0 +1,52 @@
+//
+//  Stack.m
+//  StackApplication
+//
+//  Created by Sumanta Rout on 6/29/10.
+//  Copyright 2010 Cybage. All rights reserved.
+//
+
+#import "Stack.h"
+
+
+@implementation Stack
+
+static Stack *sharedObject = nil;
+
+- (id) init
+{	
+	if( self = [super init] )
+	{
+		stackArray = [[NSMutableArray alloc] init];
+	}
+	return self;
+}
+
++(Stack *)sharedInstance
+{
+    static dispatch_once_t pred;
+    
+    dispatch_once(&pred, ^{
+        sharedObject = [[self alloc] init];
+    });
+    return sharedObject;
+
+}
+
+-(void) push:(id)object
+{
+	[stackArray addObject:object];
+}
+
+-(id) pop
+{
+	if([stackArray count] >0)
+	{		
+		id object = [stackArray lastObject];
+		[stackArray removeLastObject];
+		return object;
+	}
+	return nil;
+}
+
+@end
